c7c10474f453383ed8b1a78c909216d2cf5723d1
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-21 17:30:52 +0300

    part 42

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -2,16 +2,22 @@
  * Created by ivanlazarev on 21.10.16.
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function (ComposedComponent) {
     class Authentication extends Component {
         render() {
+            console.log(this.props.authenticated);
             return <ComposedComponent {...this.props} />
         };
     }
 
-    return Authentication;
+    function mapStateToProps(state) {
+        return { authenticated: state.authenticated}
+    }
+
+    return connect(mapStateToProps)(Authentication);
 }
 
 
-//
+
